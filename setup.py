5c2de09193d0d63d1ca5addf3c57111a6cef6aa0
Hudan Studiawan <studiawan@gmail.com>
2023-07-29 14:41:49 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='pycookieparser',
+    version='0.1',
+    description='A parser for binary cookie files',
+    author='Hudan Studiawan',
+    author_email='studiawan@gmail.com',
+    url='https://github.com/studiawan/pycookieparser',
+    packages=['pycookieparser'],  
+    install_requires=['pytest', 'Sphinx'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
