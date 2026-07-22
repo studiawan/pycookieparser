b900ffaf743b5e458338c3a69ab3a2393082bf25
Juang Maulana Taruna Putra <117959461+juangmaulana@users.noreply.github.com>
2026-07-22 11:11:12 +0700

    Bump version from 0.0.3 to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='pycookieparser',
-    version='0.0.3',
+    version='0.0.2',
     description='A parser for binary cookie files',
     long_description=long_description,
     long_description_content_type='text/markdown',
